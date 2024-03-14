6fbc6c266e619c4f7343cbd551ee614e55757703
CoyotePam <008307371@coyote.csusb.edu>
2024-03-13 23:55:35 -0700

    added pics

--- Assignment_2_user_Interactions/Metric_tracker/.venv/bin/activate.fish
+++ Assignment_2_user_Interactions/Metric_tracker/.venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/pam/Desktop/Assignment_2_user_Interactions/Metric_tracker/.venv"
+set -gx VIRTUAL_ENV "/Users/pam/Desktop/GitHub/OfficialPlatformComputing/Assignment_2_user_Interactions/Metric_tracker/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
